c82c677db968904fbf048c4752028f18b0196493
john.reyes <john.reyes@mercadolibre.com.co>
2022-06-21 11:54:32 -0500

    section four: embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var john person
-
-	john.firstName = "John"
-	john.lastName = "Reyes"
-
-	fmt.Println(john)
+	john := person{
+		firstName: "John",
+		lastName:  "Reyes",
+		contact: contactInfo{
+			email:   "john@yopmail.com",
+			zipCode: 94000,
+		},
+	}
 	fmt.Printf("%+v", john)
 }
