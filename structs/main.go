ca3b2a60c6d174327399b3a01150b255746e40b9
john.reyes <john.reyes@mercadolibre.com.co>
2022-06-21 11:43:07 -0500

    section four: updating struct values

--- structs/main.go
+++ structs/main.go
@@ -1,1 +1,18 @@
-package structs
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	var john person
+
+	john.firstName = "John"
+	john.lastName = "Reyes"
+
+	fmt.Println(john)
+	fmt.Printf("%+v", john)
+}
