45c0e62a5fe401f00339555a2cf9d519c2ef62e2
john.reyes <john.reyes@mercadolibre.com.co>
2022-06-22 08:20:20 -0500

    section five: manipulating maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"black": "#000000",
-	}
+	// var colors map[string]string
+	colors := make(map[int]string)
+
+	colors[10] = "#ffffff"
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
