a6215b84209ee40e591d74c160462a5b250f76ef
john.reyes <john.reyes@mercadolibre.com.co>
2022-06-23 16:59:26 -0500

    section seven: printing site status

--- channels/main.go
+++ channels/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	links := []string{
@@ -12,6 +15,16 @@ func main() {
 	}
 
 	for _, link := range links {
-		fmt.Println(link)
+		checkLink(link)
 	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
 }
