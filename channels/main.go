1f4b794498f1a5866d5a9b2cfb95e77a0899094f
john.reyes <john.reyes@mercadolibre.com.co>
2022-06-23 19:05:18 -0500

    section seven: repeating routines

--- channels/main.go
+++ channels/main.go
@@ -19,8 +19,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -28,10 +28,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep ist up"
+	c <- link
 }
