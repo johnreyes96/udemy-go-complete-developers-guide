cff631d7bf3a8142b07084c914aa350626896252
John Reyes <jhonfer96@hotmail.com>
2024-04-08 23:42:08 -0500

    :sparkles: Feature: Add project about to print slice

--- cards/deck.go
+++ cards/deck.go
@@ -30,8 +30,12 @@ func (d deck) print() {
 	}
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) toString() string {
-	return strings.Join(d, ",")
+	return strings.Join([]string(d), ",")
 }
 
 func (d deck) saveToFile(filename string) error {
