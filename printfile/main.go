0ae23464e82b7e230e1051adb300bbf5a5f7f94c
John Reyes <jhonfer96@hotmail.com>
2024-04-09 00:36:29 -0500

    :sparkles: Section six: add project about print file

--- printfile/main.go
+++ printfile/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args)
-	fmt.Println(os.Args[1])
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, f)
 }
